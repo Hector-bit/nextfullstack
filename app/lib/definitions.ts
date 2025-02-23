c17b9319d64bbbbc0d96f1e6bfbca58c25a9f841
Hector-bit <hectormartinez.2978@gmail.com>
2025-02-22 20:34:16 -0800

    fixed definitoins files

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -1,13 +1,15 @@
 
 const invoices = [
   {
-    customer_id: customers[0].id,
+    // customer_id: customers[0].id,
+    customer_id: 0,
     amount: 15795,
     status: 'pending',
     date: '2022-12-06',
   },
   {
-    customer_id: customers[1].id,
+    // customer_id: customers[1].id,
+    customer_id: 1,
     amount: 20348,
     status: 'pending',
     date: '2022-11-14',
